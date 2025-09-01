287f2c1b96b7fd9ea285ecade5a3529dcb499768
Nathan Haynes-Magyar <njhmagyar@gmail.com>
2025-09-01 01:57:33 -0400

    Gitignore dist

--- dist/file-system-service.d.ts
+++
@@ -1,6 +0,0 @@
-import { ToolResult } from './types.js';
-export declare class FileSystemService {
-    private static directoryExists;
-    static createDirectory(path: string): Promise<ToolResult>;
-}
-//# sourceMappingURL=file-system-service.d.ts.map
--- dist/github-service.d.ts
+++
@@ -1,8 +0,0 @@
-import { GitHubRepoOptions, GitHubRepo } from './types.js';
-export declare class GitHubService {
-    private octokit;
-    constructor(token: string);
-    createRepository(options: GitHubRepoOptions): Promise<GitHubRepo>;
-    listRepositories(): Promise<GitHubRepo[]>;
-}
-//# sourceMappingURL=github-service.d.ts.map
